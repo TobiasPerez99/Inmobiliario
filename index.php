ada5ded6ad46e172c00a70c458c9dc84cfdfd354
Tobias Perez <tobiasleonel77@gmail.com>
2021-06-28 21:01:07 -0300

    Incluyendo PHP

--- index.php
+++ index.php
@@ -187,23 +187,5 @@
     </section>
 </div>
 
-<footer class="footer seccion">
+<?php include 'includes/templates/footer.php' ?>
 
-    <div class="contenedor contenedor-footer">
-        <nav class="navegacion">
-            <a href="nosotros.php">Nosotros</a>
-            <a href="anuncios.php">Anuncios</a>
-            <a href="blog.php">Blog</a>
-            <a href="contacto.php">Contacto</a>
-        </nav>
-    </div>
-
-    <p class="copyright">Todos los derechos reservados &copy;</p>
-
-</footer>
-
-
-<script src="build/js/bundle.min.js"></script>
-</body>
-
-</html>
